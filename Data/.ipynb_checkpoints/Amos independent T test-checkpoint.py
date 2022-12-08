94ff31d61c8c6fac0edcae43876016b0034bd776
tamayal <105453914+tamayal@users.noreply.github.com>
2022-12-08 08:38:02 -0600

    Added a Week 4 folder in assignments
    Not sure why I didn't think about this before but we can definitely make one folder us to submit for the week. (I mean we are at the end now but it is what it is.)

---
+++ Data/.ipynb_checkpoints/Amos independent T test-checkpoint.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[44]:
+
+
+import pandas as pd
+import numpy as np
+import scipy
+from scipy import stats
+
+
+# In[45]:
+
+
+NYR=pd.read_csv("C:/Users/Megan/Documents/data science/Data-Science-Program/DS110-Final Project/New_years_resolutions.csv")
+
+
+# In[46]:
+
+
+NYR.rename(columns={'tweet_created' : 'Date', 'tweet_text' : 'Tweet', 'tweet_category' : 'Category', 'tweet_topics' : 'Topics', 'tweet_location' : 'Location', 'tweet_state' : 'State', 'tweet_region' : 'Region', 'user_timezone' : 'TZ', 'user_gender' : 'Gender'}, inplace=True)
+
+
+# In[47]:
+
+
+NYR.head()
+
+
+# In[48]:
+
+
+NYRtrim = NYR.drop(['Location', 'retweet_count', 'TZ', 'Location'], axis=1)
+
+
+# In[49]:
+
+
+NYRtrim.head()
+
+
+# In[50]:
+
+
+NYRtrim.info()
+
+
+# In[51]:
+
+
+from scipy.stats import ttest_ind
+
+
+# In[52]:
+
+
+NYRtrim.Category.value_counts()
+
+
+# In[53]:
+
+
+## Recoding 
+
+
+# In[54]:
+
+
+def recode (series):
+    if series == "female":
+        return 0
+    if series == "male":
+        return 1
+NYRtrim['GenderR'] = NYRtrim['Gender'].apply(recode)
+
+
+# In[55]:
+
+
+def recode (series):
+    if series == "Humor":
+        return 1
+    if series == "Health & Fitness":
+        return 2
+    if series == "Personal Growth":
+        return 3
+    if series == "Finance":
+        return 4
+    if series == "Philanthropic":
+        return 5
+    if series == "Recreation & Leisure":
+        return 6
+    if series == "Time Management/Organization":
+        return 7
+    if series == "Family/Friends/Relationships":
+        return 8
+    if series == "Career":
+        return 9
+    if series == "Education/Training":
+        return 10
+NYRtrim['CategoryR'] = NYRtrim['Category'].apply(recode)
+
+
+# In[56]:
+
+
+NYRtrim.head()
+
+
+# In[57]:
+
+
+## Independent T-Test
+
+
+# In[58]:
+
+
+stats.ttest_ind(NYRtrim["CategoryR"], NYRtrim["GenderR"])
+
